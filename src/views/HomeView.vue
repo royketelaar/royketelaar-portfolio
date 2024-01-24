f5261418179d42f43c61797925217216f8867dc2
Roy Ketelaar <royketelaar@gmail.com>
2024-01-24 21:07:23 +0100

    setting up some basic content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="mb-4">
+      Driven frontend developer with also some UI/UX experience. I like to create user friendly web
+      experiences.
+    </p>
+
+    <p class="mb-4">
+      At the moment mainly focusing on getting better at TypeScript, ReactJS and GatsbyJS Other than
+      that I like traveling, improving myself and learning new stuff 💪
+    </p>
+    
+    <p class="mb-4">Experience with:</p>
+
+    <ul class="mb-4 list-disc list-inside">
+      <li class="mb-1">Git</li>
+      <li class="mb-1">HTML/CSS</li>
+      <li class="mb-1">Javascript/Typescript</li>
+      <li class="mb-1">Frameworks: React/Gatsby Vue/Nuxt Angular</li>
+      <li class="mb-1">UI design</li>
+      <li class="mb-1">UX design</li>
+      <li class="mb-1">Design thinking</li>
+    </ul>
+
+    <p>Don't worry I will get rid of this default Vue layout soon, just trying some things out 😄</p>
   </main>
 </template>
