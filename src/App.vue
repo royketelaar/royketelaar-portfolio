4d73aba4777f7eca9515310774f688051bd6d3fb
Roy Ketelaar <royketelaar@gmail.com>
2024-01-26 22:48:30 +0100

    setup basic portfolio website

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,34 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Roy Ketelaar" />
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script lang="ts">
+export default {
+  mounted() {
+    document.title = 'Roy Ketelaar - Frontend Developer'
+    const metaDescription = document.querySelector('meta[name="description"]')
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        'content',
+        'Hi there, my name is Roy Ketelaar. I am a frontend developer with a background in web design. I love learning new things, working with people and building applications that users find useful.'
+      )
+    } else {
+      const newMetaDescription = document.createElement('meta')
+      newMetaDescription.name = 'description'
+      newMetaDescription.content =
+        'Hi there, my name is Roy Ketelaar. I am a frontend developer with a background in web design. I love learning new things, working with people and building applications that users find useful.'
+      document.head.appendChild(newMetaDescription)
+    }
   }
+}
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+body {
+  font-family: 'Noto Serif', serif;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,84 @@
 <script setup lang="ts"></script>
 
+<script>
+export default {
+  data() {
+    return {
+      aboutMeText: [
+        {
+          text: 'Hi there, my name is Roy Ketelaar. I am a frontend developer with a background in web design.'
+        },
+        {
+          text: 'I love learning new things, working with people and building applications that users find useful.'
+        },
+        {
+          text: 'I enjoy the process of trial-and-error, optimizing, and creating a good user experience.'
+        },
+        {
+          text: 'To make this happen, I think from a Design Thinking perspective, i.e. first understanding the user and putting yourself in their position before actually starting the work.'
+        },
+        {
+          text: 'Outside of work, I like experiencing new things, traveling to cool places and maintaining an active lifestyle 🏋🏻🥾'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <template>
-  <main>
-    <p class="mb-4">
-      Driven frontend developer with also some UI/UX experience. I like to create user friendly web
-      experiences.
-    </p>
+  <main class="h-screen md:flex md:flex-col md:justify-center">
+    <!-- Background image -->
+    <img
+      src="../assets/images/background.jpg"
+      alt="Background"
+      class="fixed w-screen h-screen object-cover"
+    />
+    <div class="relative flex flex-row">
+      <!-- Content -->
+      <div
+      class="relative rounded-2xl text-stone-950 bg-stone-50 bg-opacity-80 top-36 p-8 pt-48 mx-8 mb-64 md:px-16 md:pb-16 md:mx-16 lg:pt-16 xl:max-w-4xl xl:mx-auto"
+      >
+        <!-- Avatar image (from the images folder, avatar.jpg) -->
 
-    <p class="mb-4">
-      At the moment mainly focusing on getting better at TypeScript, ReactJS and GatsbyJS Other than
-      that I like traveling, improving myself and learning new stuff 💪
-    </p>
-    
-    <p class="mb-4">Experience with:</p>
+        <!-- From larger screens and up, make sure the avatar is on the top right side -->
+        <img
+          src="../assets/images/avatar.jpg"
+          alt="Avatar"
+          class="absolute rounded-full w-56 h-56 transform -translate-x-1/2 -top-16 left-1/2 lg:top-0 lg:left-auto lg:right-0 lg:transform-none lg:w-64 lg:h-64 lg:m-8"
+        />
+        <div class="lg:w-2/3 lg:pr-4">
+          <!-- Name -->
+          <h1 class="font-black text-4xl text-center mb-1 lg:text-left">Roy Ketelaar</h1>
 
-    <ul class="mb-4 list-disc list-inside">
-      <li class="mb-1">Git</li>
-      <li class="mb-1">HTML/CSS</li>
-      <li class="mb-1">Javascript/Typescript</li>
-      <li class="mb-1">Frameworks: React/Gatsby Vue/Nuxt Angular</li>
-      <li class="mb-1">UI design</li>
-      <li class="mb-1">UX design</li>
-      <li class="mb-1">Design thinking</li>
-    </ul>
+          <!-- Job -->
+          <h2 class="text-xl text-center mb-6 lg:text-left">Front-end Developer</h2>
 
-    <p>Don't worry I will get rid of this default Vue layout soon, just trying some things out 😄</p>
+          <!-- About me text -->
+          <p class="text-stone-950 mb-4" v-for="(item, index) in aboutMeText" :key="index">
+            {{ item.text }}
+          </p>
+
+          <!-- Actions -->
+          <div class="flex items-center mt-12">
+            <a href="mailto:info@royketelaar.nl">
+              <button class="bg-red-900 text-white py-2 px-4 rounded-lg mr-4">Get in touch!</button>
+            </a>
+
+            <!-- Links to GitHub and LinkedIn -->
+            <a href="https://github.com/royketelaar/" target="_blank" rel="noopener noreferrer">
+              <img src="../assets/images/github.svg" alt="GitHub" class="mx-2" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/roy-ketelaar-36821b6b/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="../assets/images/linkedin.svg" alt="LinkedIn" class="mx-2" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
