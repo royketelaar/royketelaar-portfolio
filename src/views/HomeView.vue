daf957225c88ac64f750340e31c5144b47693c39
Roy Ketelaar <royketelaar@gmail.com>
2024-01-27 14:35:12 +0100

    Refactor HomeView.vue layout and styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,84 +27,78 @@ export default {
 </script>
 
 <template>
-  <!-- Background image -->
-  <img
-    src="../assets/images/background.jpg"
-    alt="Background"
-    class="fixed w-screen h-screen object-cover"
-  />
-  <main>
-    <div class="relative flex flex-row font-sans">
-      <!-- Content -->
-      <div
-        class="relative rounded-2xl text-stone-950 bg-stone-50 bg-opacity-80 top-36 p-8 pt-48 mx-8 mb-64 md:px-16 md:pb-16 md:mx-16 lg:pt-16 xl:max-w-4xl xl:mx-auto"
-      >
-        <!-- Avatar image (from the images folder, avatar.jpg) -->
+  <main class="bg-yellow-400 min-h-screen flex items-center justify-center font-sans">
+    <!-- Content -->
+    <div
+      class="relative rounded-2xl text-stone-950 bg-stone-50 bg-opacity-80 top-36 p-8 pt-48 mx-8 mb-64 md:px-16 md:pb-16 md:mx-16 lg:pt-16 xl:max-w-4xl xl:mx-auto"
+    >
+      <!-- Avatar image (from the images folder, avatar.jpg) -->
 
-        <!-- From larger screens and up, make sure the avatar is on the top right side -->
-        <img
-          src="../assets/images/avatar.jpg"
-          alt="Avatar"
-          class="absolute rounded-full w-56 h-56 transform -translate-x-1/2 -top-16 left-1/2 lg:top-0 lg:left-auto lg:right-0 lg:transform-none lg:w-64 lg:h-64 lg:m-8"
-        />
-        <div class="lg:w-2/3 lg:pr-4">
-          <!-- Name -->
-          <h1 class="font-black text-4xl text-center mb-1 lg:text-left font-serif">Roy Test Ketelaar</h1>
+      <!-- From larger screens and up, make sure the avatar is on the top right side -->
+      <img
+        src="../assets/images/avatar.jpg"
+        alt="Avatar"
+        class="absolute rounded-full w-56 h-56 transform -translate-x-1/2 -top-16 left-1/2 lg:top-0 lg:left-auto lg:right-0 lg:transform-none lg:w-64 lg:h-64 lg:m-8"
+      />
+      <div class="lg:w-2/3 lg:pr-4">
+        <!-- Name -->
+        <h1 class="font-black text-4xl text-center mb-1 lg:text-left font-serif">
+          Roy Test Ketelaar
+        </h1>
 
-          <!-- Job -->
-          <h2 class="text-xl text-center mb-6 lg:text-left">Front-end Developer</h2>
+        <!-- Job -->
+        <h2 class="text-xl text-center mb-6 lg:text-left">Front-end Developer</h2>
 
-          <!-- About me text -->
-          <p class="text-stone-950 mb-4" v-for="(item, index) in aboutMeText" :key="index">
-            {{ item.text }}
-          </p>
+        <!-- About me text -->
+        <p class="text-stone-950 mb-4" v-for="(item, index) in aboutMeText" :key="index">
+          {{ item.text }}
+        </p>
 
-          <div class="flex flex-col mb-4">
-            <h3 class="font-bold mb-1">Languages</h3>
-            <ul class="list-disc list-inside">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript / TypeScript</li>
-            </ul>
-          </div>
+        <div class="flex flex-col mb-4">
+          <h3 class="font-bold mb-1">Languages</h3>
+          <ul class="list-disc list-inside">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript / TypeScript</li>
+          </ul>
+        </div>
 
-          <div class="flex flex-col mb-4">
-            <h3 class="font-bold mb-1">Frameworks</h3>
-            <ul class="list-disc list-inside">
-              <li>React.js</li>
-              <li>Gatsby.js</li>
-              <li>Vue.js</li>
-              <li>Node.js</li>
-            </ul>
-          </div>
+        <div class="flex flex-col mb-4">
+          <h3 class="font-bold mb-1">Frameworks</h3>
+          <ul class="list-disc list-inside">
+            <li>React.js</li>
+            <li>Gatsby.js</li>
+            <li>Vue.js</li>
+            <li>Node.js</li>
+          </ul>
+        </div>
 
-          <div class="flex flex-col mb-4">
-            <h3 class="font-bold mb-1">Design & Methodology</h3>
-            <ul class="list-disc list-inside">
-              <li>UI/UX Design</li>
-              <li>Design Thinking</li>
-              <li>SCRUM</li>
-            </ul>
-          </div>
+        <div class="flex flex-col mb-4">
+          <h3 class="font-bold mb-1">Design & Methodology</h3>
+          <ul class="list-disc list-inside">
+            <li>UI/UX Design</li>
+            <li>Design Thinking</li>
+            <li>SCRUM</li>
+          </ul>
+        </div>
 
-          <!-- Actions -->
-          <div class="flex items-center mt-12">
-            <a href="mailto:info@royketelaar.nl">
-              <button class="bg-red-900 text-white py-2 px-4 rounded-lg mr-4">Get in touch!</button>
-            </a>
+        <!-- Actions -->
+        <div class="flex items-center mt-12">
+          <a href="mailto:info@royketelaar.nl">
+            <button class="bg-red-900 text-white py-2 px-4 rounded-lg mr-4">Get in touch!</button>
+          </a>
 
-            <!-- Links to GitHub and LinkedIn -->
-            <a href="https://github.com/royketelaar/" target="_blank" rel="noopener noreferrer">
-              <img src="../assets/images/github.svg" alt="GitHub" class="mx-2" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/roy-ketelaar-36821b6b/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src="../assets/images/linkedin.svg" alt="LinkedIn" class="mx-2" />
-            </a>
-          </div>
+          <!-- Links to GitHub and LinkedIn -->
+          <a href="https://github.com/royketelaar/" target="_blank" rel="noopener noreferrer">
+            <img src="../assets/images/github.svg" alt="GitHub" class="mx-2" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/roy-ketelaar-36821b6b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="../assets/images/linkedin.svg" alt="LinkedIn" class="mx-2" />
+          </a>
         </div>
       </div>
     </div>
